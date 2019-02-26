0540ba43237636e59f59331d5a686d47b9f1a1cf
kevin <kevin_202016@outlook.es>
2019-02-25 23:48:04 -0600

    logo puesto en natvar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ asset('images/logo.png') }}" alt="" style="width: 32%">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -41,11 +41,14 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}" style="color: white;  font-size: 22px;">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item" style="color: white;  font-size: 22px; margin-top: 5px;">
+                                /
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}" style="color: white; font-size: 22px;">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -73,6 +76,9 @@
         </nav>
 
         <main class="py-4">
+
+
+
             @yield('content')
         </main>
     </div>
